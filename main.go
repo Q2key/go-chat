c6e09be213a208c3f4223be20a226035b53c9bf5
dmtocque <dmitry.totskiy@fastdev.se>
2024-09-24 15:14:18 +0400

    Revert

--- main.go
+++ main.go
@@ -8,12 +8,29 @@ import (
 
 func main() {
 
+	m1, _ := u.ReadContent("./templates/intro.txt")
+	m2, _ := u.ReadContent("./templates/outro.txt")
+	m3, _ := u.ReadContent("./templates/payload.txt")
+	m4, _ := u.ReadContent("./templates/prompt.txt")
+
 	openAiService := services.NewServiceFactory().MakeOpenAiService()
 	chatResponse, err := openAiService.Execute(core.GptModel40,
 		&[]core.Message{
 			{
 				core.GptRoleUser,
-				"write a haiku about ai",
+				m1,
+			},
+			{
+				core.GptRoleUser,
+				m2,
+			},
+			{
+				core.GptRoleUser,
+				m3,
+			},
+			{
+				core.GptRoleUser,
+				m4,
 			},
 		})
 
